121c94f94f624fe562b592608655ca70a038975a
vijay edwin <vedwin.dev@gmail.com>
2018-10-07 23:09:15 -0700

    racket module

---
+++ raco/libvxe/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "libvxe")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/libvxe.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(vedwin))
---
+++ raco/libvxe/scribblings/libvxe.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[libvxe
+                    racket/base]]
+
+@title{libvxe}
+@author{vedwin}
+
+@defmodule[libvxe]
+
+Package Description Here
